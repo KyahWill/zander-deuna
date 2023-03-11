29f6764cd4bfb063c1c03be9b44c3f9c7e1f082c
KyahWill <knownbutnotloved@gmail.com>
2023-03-11 23:25:19 +0800

    removed sv in eight and width

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -14,7 +14,7 @@ export default function NavBar() {
                 <hr className={styles.hrow}/>
             </div>
         </header>
-        <div className={styles.navMenu} style={{position:"fixed", left: !isOpen? "-100svw": "0svw"}}>
+        <div className={styles.navMenu} style={{position:"fixed", left: !isOpen? "-100vw": "0vw"}}>
             <div className={styles.navMenuTitle}>
                 <h2> TEST NAV Menu</h2>
                 <h2 onClick={() => {setIsOpen(false)}}  style={{position:"relative", right:"0px"}}>X </h2>
